f2adeb03f63e92f206724701578b5bdd6586cb1c
Marina Elizarova <seagirl1110@gmail.com>
2024-08-08 17:07:59 +0300

    feat: add component Greeting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Greeting from './components/Greeting';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Greeting name="Alex" />
+    </div>
+  );
 }
 
 export default App;
